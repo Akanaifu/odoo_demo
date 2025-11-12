1c5f19bbbcf87154d18b32965c9445ae58ee73f9
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-11-12 16:21:13 +0100

    [FIX] description -> name

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -29,7 +29,7 @@ class TestUsers(TransactionCase):
                 }
             )
             with self.assertRaises(Exception):
-                user.description = None
+                user.name = None
 
     def test_description_one_line(self):
         with self.assertRaises(ValueError) as error_catcher:
